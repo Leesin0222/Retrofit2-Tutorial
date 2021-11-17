f16774a36af35390ad8acda82aeeed6fa2cc10f0
Leesin0222 <dragon02229@naver.com>
2021-11-17 23:37:16 +0900

    📑 :: Request & Error Handling 만듬

--- retrofittutorial/app/src/main/java/com/yongjincompany/retrofittutorial/MainActivity.kt
+++ retrofittutorial/app/src/main/java/com/yongjincompany/retrofittutorial/MainActivity.kt
@@ -2,10 +2,52 @@ package com.yongjincompany.retrofittutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.yongjincompany.retrofittutorial.databinding.ActivityMainBinding
+import retrofit2.HttpException
+import java.io.IOException
 
+const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupRecyclerView()
+
+        lifecycleScope.launchWhenCreated {
+            binding.progressBar.isVisible = true
+            val response = try {
+                RetrofitInstance.api.getTodos()
+            } catch (e: IOException) {
+                Log.e(TAG, "IOException, 인터넷연결 해라")
+                binding.progressBar.isVisible = false
+                return@launchWhenCreated
+            } catch (e: HttpException) {
+                Log.e(TAG, "HttpException, 응답이 어디갔을까요?")
+                return@launchWhenCreated
+            }
+            if (response.isSuccessful && response.body() != null) {
+                todoAdapter.todos = response.body()!!
+            } else {
+                Log.e(TAG, "Responce가 실패합니다")
+            }
+            binding.progressBar.isVisible = false
+        }
+    }
+
+    private fun setupRecyclerView() = binding.rvTodos.apply {
+        todoAdapter = TodoAdapter()
+        adapter = todoAdapter
+        layoutManager = LinearLayoutManager(this@MainActivity)
     }
 }
